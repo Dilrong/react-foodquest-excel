659d342a1dec85af1e70f05480bf03673bc8410b
dilrong <wet932@naver.com>
2018-10-23 17:04:31 +0900

    change text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css'
 const Footer = () => (
     <footer className='footer' >
         <p className='text'>
-        회사명: FOODQUEST | 대표자: 전재윤 | 사업자등록번호: 440-01-00485<br/>
+        회사명: FOODQUEST | 대표자: 전재윤 | 사업자등록번호: 716-20-00724<br/>
         주소: 인천광역시 연수구 청학동 535-3 지하 | 홈페이지: www.foodquest.co.kr<br/>
         전화번호 : 010-6734-7345 | 팩스: 032-232-1562<br/>
         Copyright 2018. FOODQUEST Co. All rights reserved.
